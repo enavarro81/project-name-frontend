00eb78a63cb3e72047b2bc76592ff278975fe679
Esteban Navarro <esteban.navarro.orellana@gmail.com>
2023-12-30 18:58:30 -0500

    Feature: Instalación de CRA

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import Search from '../Search/Search';
+
+function Main() {
+  return (
+    <div className='page'>
+      <Search />
+      <main className='content'></main>
+    </div>
+  );
+}
+
+export default Main;
